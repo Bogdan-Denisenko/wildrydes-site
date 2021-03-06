2af3a5c4932245de15bcdc12fbf8aab360f71f7a
Bogdan_Denisenko <denisenko.ba@edu.spbstu.ru>
2021-03-06 22:16:35 +0300

    Change userPool 2

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. eu-west-1_puzqFJBiu
-        userPoolClientId: '', // e.g. 4b3bvch99ftq39od39mc0lui9c
-        region: '' // e.g. eu-west-1
+        userPoolId: 'eu-west-1_puzqFJBiu',
+        userPoolClientId: '4b3bvch99ftq39od39mc0lui9c',
+        region: 'eu-west-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
